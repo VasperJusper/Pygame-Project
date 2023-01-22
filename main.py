3376b1b93febb302ad6d838b2b6724188bc14838
VasperJusper <1860039@gmail.com>
2023-01-22 17:20:27 +0300

    Version 0.2 fancy turrets update + bug fixes

--- main.py
+++ main.py
@@ -3,24 +3,46 @@ import sys
 import os
 import math
 
-
-TILE_SIZE = 128
+pygame.init()
+pygame.font.init()
+test_font = pygame.font.SysFont('couriernew', 30)
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen_width = screen.get_width()
+screen_height = screen.get_height()
+background = pygame.image.load(os.path.join('Data', 'BGlava.png'))
+background = pygame.transform.scale(background, (screen_width, screen_height))
+pygame.mouse.set_visible(False)
+TILE_SIZE = (screen_width + screen_height) // 25
 BRUSH = 'h'
+GAMEMODE = 'building'
 tile_images = {
-    '0': pygame.image.load(os.path.join('Data', 'Blank.png')),
-    'furnace': pygame.image.load(os.path.join('Data', 'furnace.png')),
-    'drill': pygame.image.load(os.path.join('Data', 'drill.png')),
-    'heater': pygame.image.load(os.path.join('Data', 'heater.png'))
+    '0': pygame.image.load(os.path.join('Data', 'tile_sprites', 'Blank.png')),
+    'furnace': pygame.image.load(os.path.join('Data', 'tile_sprites', 'furnace.png')),
+    'drill': pygame.image.load(os.path.join('Data', 'tile_sprites', 'drill.png')),
+    'heater': pygame.image.load(os.path.join('Data', 'tile_sprites', 'heater.png')),
+    'turret': pygame.image.load(os.path.join('Data', 'turret_sprites', '1.png'))
 }
 tile_codes = {
     '0': '0',
     'f': 'furnace',
     'd': 'drill',
-    'h': 'heater'
+    'h': 'heater',
+    't': 'turret'
 }
+modes = ['building', 'turret control']
 for e in tile_images:
     if e != '0':
         tile_images[e] = pygame.transform.scale(tile_images[e], (TILE_SIZE, TILE_SIZE))
+
+turret_sprites = []
+
+debug = 0
+
+for i in range(1, 65):
+    t_frame = pygame.transform.scale(pygame.image.load(os.path.join('Data', 'turret_sprites', f'{i}.png')),
+                                     (TILE_SIZE * 2, TILE_SIZE * 2))
+    turret_sprites.append(t_frame)
 
 
 class Crosshair(pygame.sprite.Sprite):
@@ -38,16 +60,16 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(tiles_group)
         self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(pos_x * TILE_SIZE, pos_y * TILE_SIZE)
         self.tile_type = tile_type
+        self.rect = self.image.get_rect().move(pos_x * TILE_SIZE, pos_y * TILE_SIZE)
         self.recharge = 0
+        self.angle = 0
         self.x = pos_x
         self.y = pos_y
 
     def update(self):
         global resources
         self.tile_type = tile_codes[level[self.y - offset_y][self.x - offset_x]]
-        self.image = tile_images[self.tile_type]
         if self.tile_type == 'heater':
             resources['elec'] += 0.1
         elif self.tile_type == 'drill':
@@ -67,18 +89,27 @@ class Tile(pygame.sprite.Sprite):
                     self.recharge = 200
             else:
                 self.recharge -= 1
+        if self.tile_type == 'turret':
+            self.handle_turret()
+            self.rect = self.image.get_rect().move(self.x * TILE_SIZE - TILE_SIZE // 2,
+                                                   self.y * TILE_SIZE - TILE_SIZE // 2)
+        else:
+            self.image = tile_images[self.tile_type]
+            self.rect = self.image.get_rect().move(self.x * TILE_SIZE, self.y * TILE_SIZE)
 
+    def handle_turret(self):
+        global debug
+        if GAMEMODE == 'turret control':
+            mouse_x, mouse_y = crosshair.rect.center
+            rel_x, rel_y = mouse_x - self.rect.center[0] - TILE_SIZE // 2, mouse_y - self.rect.center[1] - TILE_SIZE // 2
+            self.angle = math.degrees(math.atan2(rel_y, rel_x)) - 270
+            if self.angle < 0:
+                self.angle += 360
+            if self.angle < 0:
+                self.angle += 360
+            debug = self.angle
+        self.image = turret_sprites[int(self.angle / 5.625)]
 
-pygame.init()
-pygame.font.init()
-test_font = pygame.font.SysFont('Comic Sans MS', 30)
-clock = pygame.time.Clock()
-screen_width = 1920
-screen_height = 1080
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-background = pygame.image.load(os.path.join('Data', 'BGlava.png'))
-background = pygame.transform.scale(background, (screen.get_width(), screen.get_height()))
-pygame.mouse.set_visible(False)
 
 level_width = 10
 level_height = 6
@@ -111,11 +142,23 @@ def create_level(width, height):
 
 def get_click(pos):
     cell = (pos[0] - offset_x * TILE_SIZE) // TILE_SIZE, (pos[1] - offset_y * TILE_SIZE) // TILE_SIZE
-    if 0 <= cell[0] <= level_width - 1 and 0 <= cell[1] <= level_height - 1:
-        level[cell[1]][cell[0]] = BRUSH
+    if GAMEMODE == 'building':
+        if 0 <= cell[0] <= level_width - 1 and 0 <= cell[1] <= level_height - 1:
+            level[cell[1]][cell[0]] = BRUSH
+
+
+def ui_draw():
+    txt_y = screen_height - 100
+    screen.blit(test_font.render(f'Electricity: {math.floor(resources["elec"])}', False, (255, 255, 255)), (100, txt_y))
+    screen.blit(test_font.render(f'Gamemode: {GAMEMODE}', False, (255, 255, 255)), (100, 100))
+    screen.blit(test_font.render(f'Raw ore: {resources["raw_ore"]}', False, (255, 255, 255)), (600, txt_y))
+    screen.blit(test_font.render(f'Smelted ore: {resources["smelt_ore"]}', False, (255, 255, 255)), (1100, txt_y))
+    screen.blit(test_font.render(f'FPS: {int(clock.get_fps())}', False, (255, 255, 255)), (1600, txt_y))
+    screen.blit(test_font.render(f'Debug: {debug}', False, (255, 255, 255)), (2100, txt_y))
 
 
 create_level(level_width, level_height)
+m = 0
 
 while True:
     for event in pygame.event.get():
@@ -133,12 +176,16 @@ while True:
                 BRUSH = 'd'
             elif event.key == pygame.key.key_code('3'):
                 BRUSH = 'f'
+            elif event.key == pygame.key.key_code('4'):
+                BRUSH = 't'
+            elif event.key == pygame.key.key_code('m'):
+                m = (m + 1) % len(modes)
+                GAMEMODE = modes[m]
     screen.blit(background, (0, 0))
     tiles_group.draw(screen)
     ui_group.draw(screen)
     ui_group.update()
+    ui_draw()
     tiles_group.update()
-    screen.blit(test_font.render(f'Electricity: {math.floor(resources["elec"])}', False, (255, 255, 255)), (100, 1000))
-    screen.blit(test_font.render(f'Raw ore: {resources["raw_ore"]}', False, (255, 255, 255)), (600, 1000))
-    screen.blit(test_font.render(f'Smelted ore: {resources["smelt_ore"]}', False, (255, 255, 255)), (1500, 1000))
     pygame.display.update()
+    clock.tick(60)
